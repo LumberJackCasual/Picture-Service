8defba43463470fc7a2f312bf7d637eec897588a
Patrice Cottman <patrice.cottman@gmail.com>
2021-01-18 04:10:01 +0000

    Following improvements made to modal: 1.  Header was taken out of div #app and I used DOM manipulation to create and attach a header with all the needed buttons and text to the body 2.  Styled header, vertical scroll and horizontal scroll to match overstock; the only thin not completly done is the vertical scroll buttons.  I still need to figure out how to tackle that issue. There are several bugs that still need work: 1.  close button needs an svg 'x' mark or picture of some sort. 2.  handler needed in horizontal scroll thats set img to current index instead of the default zero when modal is activated. 3.  same above for vertical scroll.

--- Client/src/components/PicModule.jsx
+++ Client/src/components/PicModule.jsx
@@ -32,15 +32,31 @@ class PicModule extends React.Component {
   modalView() {
     const defaultStyle = document.getElementById('defaultStyle');
     const modalStyle = document.getElementById('modalStyle');
+    const header = document.createElement('div');
+    header.id = 'modalHeader';
+    const headerCountBTN = document.createElement('button');
+    headerCountBTN.className = 'headerCountBTN';
+    headerCountBTN.innerText = `${this.state.currentIndex}/ ${this.state.largePics.length}`;
+    const text = document.createElement('h3');
+    text.innerText = `${this.state.description}`;
+    const closeBTN = document.createElement('button');
+    closeBTN.className = 'closeBTN';
+    closeBTN.addEventListener('click', this.modalView);
+    const svg = document.createElement('svg');
 
     if (this.state.modalView === false) {
-      this.setState({modalView: true});
+      this.setState({ modalView: true });
       modalStyle.href = 'modal.css';
       defaultStyle.href = '';
+      header.appendChild(headerCountBTN);
+      header.appendChild(text);
+      header.appendChild(closeBTN);
+      document.body.prepend(header);
     } else {
-      this.setState({modalView: false});
+      this.setState({ modalView: false });
       modalStyle.href = '';
       defaultStyle.href = 'styles.css';
+      document.body.removeChild(document.body.childNodes[0]);
     }
   }
 
@@ -59,9 +75,11 @@ class PicModule extends React.Component {
         }
         {this.state.modalView === true &&
         <div>
-            <h3>Hi from Modal!</h3>
-            <button id="closeModalButton" type="button" onClick={this.modalView}>close</button>
-          </div>
+          <div id="flex-container">
+              <HorizontalScroll largePics={this.state.largePics} modalView={this.modalView} idxSync={this.idxSync} />
+              <VerticalScroll thumbnails={this.state.thumbnails} focus={this.state.currentIndex} />
+           </div>
+        </div>
         }
       </div>
     );
