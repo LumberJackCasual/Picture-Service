05e99f1ff94d095e93e97f77862b2f0f7b95c3c3
Patrice Cottman <patrice.cottman@gmail.com>
2021-01-21 00:57:02 -0500

    Changed placement of mouse events to stop infinite mouseenter and leave events and flashing render issues.  set background size to use ratio of the horizontal container and the lens div.  Now working out changing the position.

--- Client/src/components/HorizontalScroll.jsx
+++ Client/src/components/HorizontalScroll.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const HorizontalScroll = ({ largePics, modalView, idxSync, currentIndex }) => {
+const HorizontalScroll = ({ largePics, modalView, idxSync, currentIndex, fullScreen }) => {
   const index = currentIndex;
   const arr = largePics;
+  const img = arr[index];
+
 
   const onClickRightHandler = () => {
     if (index >= arr.length - 1) {
@@ -25,27 +27,36 @@ const HorizontalScroll = ({ largePics, modalView, idxSync, currentIndex }) => {
   const mouseEnter = () => {
     const spotlight = document.getElementById('zoomLens');
     spotlight.style.display = 'block';
+    const zoomModal = document.getElementById('zoomPic');
+    zoomModal.style.display = 'block';
   }
 
   const mouseMoveHandler = (event) => {
-    //console.dir(event.target);
-    //console.log(`${event.pageX} and ${event.pageY}`);
     const spotlight = document.getElementById('zoomLens');
-    const mouseY = event.pageY;
-    const mouseX = event.pageX;
-    spotlight.style.top = mouseY - 190 + 'px';
-    spotlight.style.left = mouseX - 340 + 'px';
+    spotlight.style.top = (event.pageY- 180) + 'px';
+    spotlight.style.left = (event.pageX - 360) + 'px';
+    // console.log (`Y/Top: ${event.pageY}, X/Left: ${event.pageX}`)
+    const zoomPic = document.getElementById('zoomPic');
+    zoomPic.style.backgroundImage = `url(${img})`;
+    const cx = zoomPic.offsetWidth / spotlight.offsetWidth;
+    const cy = zoomPic.offsetHeight / spotlight.offsetHeight;
+    zoomPic.style.backgroundSize = `${498 * cx}px ${498 * cy}px`;
 
   }
  const mouseLeave = () => {
     const spotlight = document.getElementById('zoomLens');
     spotlight.style.display = 'none';
+    const zoomModal = document.getElementById('zoomPic');
+    zoomModal.style.display = 'none';
   }
 
   return (
-    <div id="picture-module-horizontal-scroll">
-      <img src={arr[index]} alt="pretty stuff" onClick={activateFullView} role="presentation" onMouseEnter={mouseEnter} onMouseMove={mouseMoveHandler} onMouseLeave = {mouseLeave} />
-      <div id="zoomLens" />
+    <div id="picture-module-horizontal-scroll" onMouseEnter={!fullScreen ?(event) => mouseEnter(event): null} onMouseMove= {!fullScreen ?mouseMoveHandler: null} onMouseLeave = {mouseLeave} >
+      <div >
+      <img src={img} alt="pretty stuff"  role="presentation" />
+      </div>
+      <div id="zoomLens" onClick={activateFullView} />
+      <div id ="zoomPic" display={!fullScreen ? 'none' : 'block' }></div>
       <button className="horizontal-scroll-rightClick" onClick={onClickRightHandler} type="button">
         <svg color="#2F3337" width="24" height="24" viewBox="0 0 24 24" stroke="#2F3337" fill="#2F3337">
           <title>Chevron Right</title>
